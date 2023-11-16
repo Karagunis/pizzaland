12682f7e98a552f6591006f5eb9df3977bb55530
markok <markoka93@gmail.com>
2023-11-16 08:37:32 +0100

    first push

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,10 +1,53 @@
+"use client";
 // css
-import './globals.css';
+import "./globals.css";
+// components
+import Nav from "./components/Nav";
+import CartMobileIcon from "./components/CartMobileIcon";
+import CartMobile from "./components/CartMobile";
+import CartDesktop from "./components/CartDesktop";
+import Footer from "./components/Footer";
+
+// provider
+import CartProvider from "./context/CartContext";
+
+// import next fonts
+import { Bangers, Quicksand, Roboto_Condensed } from "next/font/google";
+import { useState } from "react";
+
+
+const quicksand = Quicksand({
+  subsets: ["latin"],
+  variable: "--font-quicksand",
+});
+
+const bangers = Bangers({
+  subsets: ["latin"],
+  variable: "--font-bangers",
+  weight: ["400"],
+});
+
+const robotoCondensed = Roboto_Condensed({
+  subsets: ["latin"],
+  variable: "--font-robotoCondensed",
+  weight: ["300", "400", "700"],
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
-    </html>
+    <CartProvider>
+      <html lang="en">
+        <body
+          className={`${quicksand.variable} ${bangers.variable} ${robotoCondensed.variable} font-quicksand`}
+        >
+          <Nav />
+          <CartMobileIcon />
+          <CartMobile />
+          {children}
+          <CartDesktop />
+          <Footer />
+        </body>
+      </html>
+    </CartProvider>
   );
 }
